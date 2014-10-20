61a9422c9da85d25491a958ddc55d138cb0f5c17
Nicholas Wright <dozedoffagain@gmail.com>
2014-10-20 19:06:48 +0200

    Fixed regex that triggers python RE bug

--- python/net/torrent.py
+++ python/net/torrent.py
@@ -3,7 +3,7 @@ import re
 class MagnetFinder:
 	def __init__ (self):
 		self.full_link_pattern = re.compile('magnet:\?xt=urn:btih:[0-9a-z]*&dn=.*&tr=[a-zA-Z0-9%.]*')
-		self.grouped_link_pattern = re.compile('((magnet:\?){1}(xt=urn:btih:){1}([0-9a-z]*){1}(&dn=.*){1}(&tr=[a-zA-Z0-9%.]*)+)')
+		self.grouped_link_pattern = re.compile('((magnet:\?)(xt=urn:btih:)([0-9a-z]*)(&dn=.*)(&tr=[a-zA-Z0-9%.]*)+)')
 		
 	def get_magnet_links (self,raw_html, magnet_terminator):
 		selection=re.findall(self.full_link_pattern,raw_html)
