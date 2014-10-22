717925177a9648c8e50cb15585d5ca854809cf66
dozedoff <dozedoffagain@gmail.com>
2014-10-22 06:31:16 +0200

    Added install script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='commonpy',
+ version='0.1',
+ description='Common code used in other projects',
+ author='Nicholas Wright',
+ author_email='dozedoffagain@gmail.com',
+ url='https://github.com/dozedoff/commonpy',
+ packages=['commonpy'],
+)
