1c2729364ba14a6a4454bf4540583c9ee1003bfa
Nicholas Wright <dozedoffagain@gmail.com>
2014-10-25 17:57:46 +0200

    Piping filefrag stderr to stdout

--- commonpy/util/file.py
+++ commonpy/util/file.py
@@ -3,6 +3,7 @@ import os
 import re
 from subprocess import check_output
 from subprocess import call
+import subprocess as subprocess
 
 extent_pattern = re.compile('(: )([0-9]*)( extents? found)')
 
@@ -16,7 +17,7 @@ def walk_directory (root_path):
 	return file_paths
 
 def get_extents (file_path):
-	output=check_output(["filefrag", file_path])
+	output=check_output(["filefrag", file_path],stderr=subprocess.STDOUT)
 	return _regex_extent_count(output)
 
 def _regex_extent_count (output):
